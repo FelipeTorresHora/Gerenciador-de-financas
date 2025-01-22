a66ee9c6b16fb0e83fb08009ed2306310be3103b
Felipe <fhora93@gmail.com>
2025-01-21 23:28:23 -0300

    Escopo projeto

--- gerenciador_orcamento/settings.py
+++ gerenciador_orcamento/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'orcamento',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SHEET2API_URL = 'https://sheet2api.com/v1/iHLaXYEkR9GG/db-orcamento'
