019c5f2565d967621caa9262747e356337ed5a34
Felipe <fhora93@gmail.com>
2025-04-20 23:30:26 -0300

    feat: add and delete transaction

--- orcamento/urls.py
+++ orcamento/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('save-expense/', views.save_expense, name='save_expense'),
-    path('update-chart/', views.update_chart, name='update_chart'),
+    path('delete-transaction/', views.delete_transaction, name='delete_transaction'),
     path('transacoes/', views.transacoes, name='transacoes'),  
     path('ia-agent/', views.ia_agent, name='ia_agent'),
 ]
